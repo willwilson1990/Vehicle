53b95279fadcca4c430a2afc75287c6ae5da1dfd
willwilson1990 <54276192+willwilson1990@users.noreply.github.com>
2019-09-12 10:23:22 -0500

    Add files via upload

--- automobile.java
+++ automobile.java
@@ -1,14 +1,48 @@
 package Vehicle;
 
-public class Automobile extends VehicleWILL {
+public class automobile extends VehicleWILL {
 	
 	private boolean Trunk;
 	private boolean Ac;
 	private boolean Tint;
 	private String Owner;
 	private String Title;
-	private String MakeAndModel;
+	private Maker AutoMake;
+	private Model AutoModel;
 	
+	automobile(){
+		super();
+		Trunk = true;
+		Ac = true;
+		Tint = true;
+		Owner = "me";
+		Title = "in glovebox";
+		AutoMake = new Maker();
+		AutoModel = new Model();
+		}
+	
+	automobile(int p, int f, int m, Maker vM, Model vMod ){
+		super(p,f,m);
+		Trunk = true;
+		Ac = true;
+		Tint = true;
+		Owner = "me";
+		Title = "in glovebox";
+		AutoMake = vM;
+		AutoModel = vMod;
+	}
+	
+	automobile(int p, int f, int m, int d, int drs, int t, String c, int w, int y, int tm, Maker vM, Model vMod){
+		super(p, f, m, d, drs, t, c, w, y, tm);
+		Trunk = true;
+		Ac = true;
+		Tint = true;
+		Owner = "me";
+		Title = "in glovebox";
+		AutoMake = vM;
+		AutoModel = vMod;
+	}
+
 	public boolean isTrunk() {
 		return Trunk;
 	}
@@ -41,18 +75,26 @@ public class Automobile extends VehicleWILL {
 	}
 	
 	int getNumWheels () {
+		String[] trimPackage = AutoModel.getPackageType();
+		 int wheelCount = 0;
+		 try {
+			 wheelCount = Integer.parseInt(trimPackage[1]);
+		 }
+		 catch(Exception e) {
+			 System.out.println("BAD TRIM PACKAGE ");
+		 }
+		//return Integer.parseInt(PackageType[1]) ;
+		 return wheelCount;
+	}
+	
+	int getNumDoors() {
 		return ;
 	}
 	
-	int getNUmDoors() {
+	public void getMakeAndModel() {     //adds abstract method from VehicleWILL
 		return ;
 	}
+		
+
 	
-	String getMakeAndModel(String ModelName, String company) {
-		return MakeAndModel;
-	}
-	
-	String setMakeAndModel(String newMakeAndModel) {
-		MakeAndModel = newMakeAndModel ;
-	}
 }
